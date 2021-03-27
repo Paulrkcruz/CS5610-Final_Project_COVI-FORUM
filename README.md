653b37a6a4490585dd9667e17f920a6eb42ef70d
Paul Cruz <cruz.p@husky.neu.edu>
2021-03-27 15:44:54 -0700

    spring boot frontend & react backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS5610-Final_Project_COVI-FORUM
-Final Project for CS5610 - Web Development - at Northeastern University (NEU)
---
+++ coviforum/src/test/java/com/coviforum/coviforum/CoviforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coviforum.coviforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoviforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
