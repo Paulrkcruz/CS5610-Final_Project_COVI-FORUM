e74892c37ffba6bb5a547f6b7523d2f6a4bc13a7
Paul Cruz <cruz.p@husky.neu.edu>
2021-03-31 17:55:09 -0700

    backend setup

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <Link to={"/"} className="navbar-brand">
-            Coviforum
+            CoviForum
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
