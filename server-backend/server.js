7be9463be16f040680fd101c32ad66acbb5a0d40
Paul Cruz <cruz.p@husky.neu.edu>
2021-03-31 20:06:11 -0700

    implement server-backend

--- server-backend/server.js
+++ server-backend/server.js
@@ -4,8 +4,34 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-    origin: "http://localhost:8081"
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+const corsOptions = {
+  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
@@ -18,11 +44,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to CoviForum." });
+  res.json({ message: " > Express Node Server is now running for CoviForum on port 8081." });
 });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(` > Server is running on port ${PORT}.`);
 });
+
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,27 +1,69 @@
-import axios from "axios";
-import authHeader from "./auth-header";
+import axios from 'axios';
+import { deleteCookie, getCookie, setCookie } from "./cookie.service";
+import authHeader from './auth-header';
 
-const API_URL = "http://localhost:8080/api/test/";
+const API_URL = 'http://localhost:8080/api/test/';
 
-const getPublicContent = () => {
-  return axios.get(API_URL + "all");
-};
+class UserService {
 
-const getUserBoard = () => {
-  return axios.get(API_URL + "user", { headers: authHeader() });
-};
+  getPublicContent() {
+    return axios.get(API_URL + 'all');
+  }
 
-const getModeratorBoard = () => {
-  return axios.get(API_URL + "mod", { headers: authHeader() });
-};
+  getUserBoard() {
+    return axios.get(API_URL + 'user', {headers: authHeader()});
+  }
 
-const getAdminBoard = () => {
-  return axios.get(API_URL + "admin", { headers: authHeader() });
-};
+  getModeratorBoard() {
+    return axios.get(API_URL + 'mod', {headers: authHeader()});
+  }
 
-export default {
-  getPublicContent,
-  getUserBoard,
-  getModeratorBoard,
-  getAdminBoard,
-};
+  getAdminBoard() {
+    return axios.get(API_URL + 'admin', {headers: authHeader()});
+  }
+
+
+}
+const
+    COOKIE_NAME = "coviforum";
+// Should have the following properties
+/*
+    this.id
+    this.name
+    this.created
+    this.updated
+    this.email
+    this.active
+*/
+  const addUser = (user) => {
+    Object.assign(currentUser, user);
+    setCookie(COOKIE_NAME, JSON.stringify(currentUser), 1);
+  };
+
+  const getUser = () => {
+    if (!initialLoad) {
+      let cookie = getCookie(COOKIE_NAME);
+      if (cookie) {
+        try {
+          cookie = JSON.parse(cookie);
+        } catch (error) {
+          console.log("Failed to load cached user");
+        }
+        addUser(cookie);
+      }
+      initialLoad = true;
+    }
+    return currentUser;
+  };
+
+  const logOut = () => {
+    Object.assign(currentUser);
+    deleteCookie(COOKIE_NAME);
+    window.location.href = "/";
+  };
+
+  let initialLoad = false;
+  const currentUser = {};
+
+export default new UserService();
+export { currentUser, addUser, getUser, logOut };
