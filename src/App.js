40f69c748b18025c68d97d3f0dc1ab1b48f27d46
Paul Cruz <cruz.p@husky.neu.edu>
2021-04-01 19:54:18 -0700

    bootstrap-navbar

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
 
   return (
       <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <nav className="navbar navbar-dark bg-primary">
           <Link to={"/"} className="navbar-brand">
             CoviForum
           </Link>
