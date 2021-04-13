8ac08d9d6dc4d0b40d89bec4018793dea50b4b50
Paul Cruz <cruz.p@husky.neu.edu>
2021-04-12 20:18:14 -0700

    heroku deployment-express-server-mysql-setup

--- server-backend/app/config/db.config.js
+++ server-backend/app/config/db.config.js
@@ -2,5 +2,12 @@ module.exports = {
   HOST: "us-cdbr-east-03.cleardb.com",
   USER: "b433a5a4586959",
   PASSWORD: "afead08f",
-  DB: "heroku_d12c9954cf94e81"
+  DB: "heroku_d12c9954cf94e81",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
