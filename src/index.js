059af67b87ef70fc54983e0361f2fd2a47675892
Paul Cruz <cruz.p@husky.neu.edu>
2021-04-14 09:54:37 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { BrowserRouter } from "react-router-dom";
-//
-// import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
-//
-// ReactDOM.render(
-//     <BrowserRouter>
-//         <App />
-//     </BrowserRouter>,
-//     document.getElementById("root")
-// );
-//
-// serviceWorker.unregister();
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -29,3 +14,19 @@ ReactDOM.render(
 );
 
 serviceWorker.unregister();
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
+//
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+//
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>,
+//     document.getElementById("root")
+// );
+//
+// serviceWorker.unregister();
