efcf1d46d0f770d90b299b3214cd77227b185328
Paul Cruz <cruz.p@husky.neu.edu>
2021-04-17 17:24:46 -0700

    heroku ui

---
+++ src/components/boards.list_pub.js
@@ -0,0 +1,246 @@
+import React, { Component } from "react";
+import BoardsDataService from "../services/boards.service";
+import { Link } from "react-router-dom";
+import Pagination from "@material-ui/lab/Pagination";
+
+export default class BoardsList extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+    this.retrieveBoards = this.retrieveBoards.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveBoards = this.setActiveBoards.bind(this);
+    this.removeAllBoards = this.removeAllBoards.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
+    this.handlePageSizeChange = this.handlePageSizeChange.bind(this);
+
+    this.state = {
+      boards: [],
+      currentBoards: null,
+      currentIndex: -1,
+      searchTitle: "",
+
+      page: 1,
+      count: 0,
+      pageSize: 11,
+    };
+
+    this.pageSizes = [3, 6, 9, 11];
+  }
+
+  componentDidMount() {
+    this.retrieveBoards();
+  }
+
+  onChangeSearchTitle(e) {
+    const searchTitle = e.target.value;
+
+    this.setState({
+      searchTitle: searchTitle,
+    });
+  }
+
+  getRequestParams(searchTitle, page, pageSize) {
+    let params = {};
+
+    if (searchTitle) {
+      params["title"] = searchTitle;
+    }
+
+    if (page) {
+      params["page"] = page - 1;
+    }
+
+    if (pageSize) {
+      params["size"] = pageSize;
+    }
+
+    return params;
+  }
+
+  retrieveBoards() {
+    const { searchTitle, page, pageSize } = this.state;
+    const params = this.getRequestParams(searchTitle, page, pageSize);
+
+    BoardsDataService.getAll(params)
+      .then((response) => {
+        const { boards, totalPages } = response.data;
+
+        this.setState({
+          boards: boards,
+          count: totalPages,
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveBoards();
+    this.setState({
+      currentBoards: null,
+      currentIndex: -1,
+    });
+  }
+
+  setActiveBoards(boards, index) {
+    this.setState({
+      currentBoards: boards,
+      currentIndex: index,
+    });
+  }
+
+  removeAllBoards() {
+    BoardsDataService.deleteAll()
+      .then((response) => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  handlePageChange(event, value) {
+    this.setState(
+      {
+        page: value,
+      },
+      () => {
+        this.retrieveBoards();
+      }
+    );
+  }
+
+  handlePageSizeChange(event) {
+    this.setState(
+      {
+        pageSize: event.target.value,
+        page: 1
+      },
+      () => {
+        this.retrieveBoards();
+      }
+    );
+  }
+
+  render() {
+    const {
+      searchTitle,
+      boards,
+      currentBoards,
+      currentIndex,
+      page,
+      count,
+      pageSize,
+    } = this.state;
+
+    return (
+      <div className="list row">
+        <h3>Public Viewing Mode. Please Sign up or Sign in!</h3>
+        <div className="col-md-8">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by title"
+              value={searchTitle}
+              onChange={this.onChangeSearchTitle}
+            /><br></br>
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.retrieveBoards}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h4>Trending</h4>
+
+          <div className="mt-3">
+            {"Boards per Page: "}
+            <select onChange={this.handlePageSizeChange} value={pageSize}>
+              {this.pageSizes.map((size) => (
+                <option key={size} value={size}>
+                  {size}
+                </option>
+              ))}
+            </select>
+
+            <Pagination
+              className="my-3"
+              count={count}
+              page={page}
+              siblingCount={1}
+              boundaryCount={1}
+              variant="outlined"
+              shape="rounded"
+              onChange={this.handlePageChange}
+            />
+          </div>
+
+          <ul className="list-group">
+            {boards &&
+              boards.map((boards, index) => (
+                <li
+                  className={
+                    "list-group-item list-group-item- " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setActiveBoards(boards, index)}
+                  key={index} data-toggle="modal"
+                >
+                  {boards.title}
+                </li>
+              ))}
+          </ul>
+
+
+          {/*<button*/}
+          {/*  className="m-3 btn btn-sm btn-danger"*/}
+          {/*  onClick={this.removeAllBoards}*/}
+          {/*>*/}
+          {/*  Remove All*/}
+          {/*</button>*/}
+        </div>
+        <div className="col-md-6">
+          {currentBoards ? (
+            <div>
+              <h4>Boards</h4>
+              <div>
+                <label>
+                  <strong>Title:</strong>
+                </label>{" "}
+                {currentBoards.title}
+              </div>
+              <div>
+                <label>
+                  <strong>Story:</strong>
+                </label>{" "}
+                {currentBoards.description}
+              </div>
+              <div>
+                <label>
+                  <strong>Status:</strong>
+                </label>{" "}
+                {currentBoards.published ? "Published" : "Pending"}
+              </div>
+              <div className="alert-link">
+              </div>
+              </div>
+          ) : (
+              <div>
+              <br />
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
