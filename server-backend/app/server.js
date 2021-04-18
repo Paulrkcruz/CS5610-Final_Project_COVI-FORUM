29190c354a07e5dfd501e5dd081a24baf2ca37a4
Paul Cruz <cruz.p@husky.neu.edu>
2021-04-17 17:49:52 -0700

    server

--- server-backend/app/server.js
+++ server-backend/app/server.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 
 const app = express();
 
-// const corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-//
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: "https://coviforum.herokuapp.com"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
